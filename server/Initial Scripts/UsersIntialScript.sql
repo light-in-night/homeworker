dc6f9e716c8a4554a349503a75eab76a6053dfab
light-in-night <tornikeonoprishvili@gmail.com>
2019-06-10 00:31:56 +0400

    added initialSctipt and added in contextKeys

--- server/Initial Scripts/UsersIntialScript.sql
+++ server/Initial Scripts/UsersIntialScript.sql
@@ -2,9 +2,10 @@ DROP TABLE users;
 
 CREATE TABLE users (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(100) NOT NULL ,
-    last_name VARCHAR(100) NOT NULL ,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     gender VARCHAR(100),
-    email VARCHAR(100) NOT NULL UNIQUE ,
-    password VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    primary key (id)
 );
