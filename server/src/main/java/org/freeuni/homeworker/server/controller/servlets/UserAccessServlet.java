143dbf41d271f00bbe3d566334b97b81b2541a83
Guga Tkesheladze <gtkes17@freeuni.edu,ge>
2019-06-11 04:03:41 +0400

    Implemented cool header and fixed some classloader problem.

--- server/src/main/java/org/freeuni/homeworker/server/controller/servlets/UserAccessServlet.java
+++ server/src/main/java/org/freeuni/homeworker/server/controller/servlets/UserAccessServlet.java
@@ -8,43 +8,32 @@ import org.freeuni.homeworker.server.model.managers.users.UserManagerSQL;
 import org.freeuni.homeworker.server.model.objects.user.User;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "UserAccessServlet", urlPatterns = {"/getusers","/getUsers"})
 public class UserAccessServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET");
 
         Object obj = request.getServletContext().getAttribute(ContextKeys.USER_MANAGER);
-        System.out.println(obj.getClass().getClassLoader());
-        System.out.println(UserManager.class.getClassLoader());
-        
-        // THIS LINE.
-        
-        UserManagerSQL userManager = (UserManagerSQL) obj;
+        UserManager userManager = (UserManager) obj;
 
-        //TODO: HELP ! CANNOT CAST A CLASS TO ITS INTERFACE. STUPID JAVA. 
-
-        List<User> userList = new ArrayList<>();
-        userList.add(new User(0,"Givi","Racxa","pansexual","givi@gmail","lels"));
-        userList.add(new User(0,"Marco","eewq","sapiosexual","givi@gmail","dad"));
+        List<User> users = userManager.getUsers();
 
         response.setContentType("application/json;charset=UTF-8");
-
-        ServletOutputStream out = response.getOutputStream();
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        String output = gson.toJson(userList);
-        out.print(output);
+        String output = gson.toJson(users);
+        response.getWriter().write(output);
     }
 }
